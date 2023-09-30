bb80ae3463819c41abadeef730c5176517329bf1
emolchanov <no_name2@data-platform.ru>
2023-10-01 00:03:32 +0300

    first commit

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,16 @@
+import { PRODUCTS } from "../../products"
+import { Product } from "./product"
+import "./shop.css"
+
+export const Shop = () => {
+	return <div className="shop" >
+		<div className="shopTitle">
+			<h1>My Shop</h1>
+		</div>
+		<div className="products">
+		{PRODUCTS.map((product)=>{
+			return <Product data={product}/>
+		})}
+		</div>
+	</div>
+}
